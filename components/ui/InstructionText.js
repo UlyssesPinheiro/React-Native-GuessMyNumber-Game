0f65961b83d4b6f6f0f3a23f0583944f2ad2025c
Ulysses Pinheiro <ulyssespinheiro00@gmail.com>
2022-06-24 14:23:15 -0300

    game over screen

--- components/ui/InstructionText.js
+++ components/ui/InstructionText.js
@@ -2,12 +2,13 @@ import React from "react";
 import { StyleSheet, Text } from "react-native";
 import Color from "../../Helper/Color";
 
-export default function InstructionText({ children }) {
-  return <Text style={styles.instructionText}>{children}</Text>;
+export default function InstructionText({ children, style }) {
+  return <Text style={[styles.instructionText, style]}>{children}</Text>;
 }
 
 const styles = StyleSheet.create({
   instructionText: {
+    fontFamily: "open-sans",
     color: Color.secondary500,
     fontSize: 24,
   },
--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,6 +1,64 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View, Image, StyleSheet } from "react-native";
+import successImg from "../assets/images/success.png";
+import PrimaryButton from "../components/ui/PrimaryButton";
+import Title from "../components/ui/Title";
+import Color from "../Helper/Color";
 
-export default function GameOverScreen() {
-  return <Text>Game is Over</Text>;
+export default function GameOverScreen({
+  roundsNumber,
+  userNumber,
+  onStartNewGame,
+}) {
+  return (
+    <View style={styles.outerContainer}>
+      <Title>Game Over!</Title>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={successImg} />
+      </View>
+      <Text style={styles.summaryText}>
+        Your phone needed{" "}
+        <Text style={styles.highLightText}>{roundsNumber}</Text> rounds to guess
+        the number <Text style={styles.highLightText}>{userNumber}</Text>.
+      </Text>
+      <PrimaryButton onPress={onStartNewGame}>Start New Game</PrimaryButton>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  outerContainer: {
+    flex: 1,
+    padding: 24,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  imageContainer: {
+    borderRadius: 200,
+    width: 350,
+    height: 350,
+    borderWidth: 3,
+    borderColor: Color.primary800,
+    overflow: "hidden",
+    margin: 36,
+  },
+
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+
+  summaryText: {
+    fontFamily: "open-sans",
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 24,
+  },
+
+  highLightText: {
+    fontFamily: "open-sans-bold",
+    color: Color.primary500,
+  },
+});
